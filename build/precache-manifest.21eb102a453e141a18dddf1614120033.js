5c9fd0384d580dd7b040edb3af6f50705c49d970
Nate Power <power2677247@gmail.com>
2021-10-13 16:32:16 -0700

    commit public and src folders

---
+++ build/precache-manifest.21eb102a453e141a18dddf1614120033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b82752b8658e2edb4fa",
+    "url": "/nate-power/comp3104/static/css/main.20caaf94.chunk.css"
+  },
+  {
+    "revision": "5b82752b8658e2edb4fa",
+    "url": "/nate-power/comp3104/static/js/main.5b82752b.chunk.js"
+  },
+  {
+    "revision": "b35673ad7ad0cf0a551f",
+    "url": "/nate-power/comp3104/static/js/runtime~main.b35673ad.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/nate-power/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7c9ab0dc8f8d5bc489d81bfc6108720c",
+    "url": "/nate-power/comp3104/index.html"
+  }
+];
