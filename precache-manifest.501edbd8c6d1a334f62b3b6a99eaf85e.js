e11446be242ce558e6b6cf70b4c7d06da36ac102
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 22:43:53 +0000

    Deploy nate-power/comp3104 to github.com/nate-power/comp3104.git:gh-pages

---
+++ precache-manifest.501edbd8c6d1a334f62b3b6a99eaf85e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5d4f3e83cfe3961c2bd",
+    "url": "/nate-power/comp3104/static/css/main.1bcdb2e4.chunk.css"
+  },
+  {
+    "revision": "c5d4f3e83cfe3961c2bd",
+    "url": "/nate-power/comp3104/static/js/main.c5d4f3e8.chunk.js"
+  },
+  {
+    "revision": "b35673ad7ad0cf0a551f",
+    "url": "/nate-power/comp3104/static/js/runtime~main.b35673ad.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/nate-power/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4bfdb0c00f514e314c8e3e5d40e1400f",
+    "url": "/nate-power/comp3104/index.html"
+  }
+];
